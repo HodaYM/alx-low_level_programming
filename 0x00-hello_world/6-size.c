fb4b92e4fb0ea0c6ec365249ce04e79a97542fd2
“Hoda <hoda27mohamed@gmail.com>
2023-07-14 15:25:11 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
 /**
-* main -Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{
-	printf("Size of a char %d byte(s)\n", sizeof(char));
-	printf("size of an int %d bytes(s)\n", sizeof(int));
-	printf("size of a long int %d bytes(s)\n", sizeof(long in));
-	prinf("size of along long int %d bytes(s)\n", sizeof(long long int));
-	printf ("size of a float %d bytes(s)\n", sizeof(float));
-	return (0);
+{       
+        int integerType;
+        char charType;
+        float floatType;
+        printf("Size of char is: %ld", sizeof(charType));
+        printf("size of int is: %ld:", sizeof(interType));
+        printf("The size of a long int: %lu.\n", (unsigned long)sizeof(c));
+        printf("Size of a long long int: %lu.\n", (unsigned long)sizeof(c));
+        printf("Size of float is: %ld", sizeof(floatType));
+        return (0);
 }
