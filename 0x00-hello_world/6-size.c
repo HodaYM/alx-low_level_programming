8714e7c4a8bee4ed21cb7ec003abcaec3196310b
“Hoda <hoda27mohamed@gmail.com>
2023-07-14 14:36:30 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include<stdio>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int integerType;
+	char charType;
+	float floatType;
+
+	printf("Size of char is: %ld", sizeof(charType));
+	printf("size of int is: %ld:", sizeof(interType));
+	printf("The size of a long int: %lu.\n", (unsigned long)sizeof(c));
+        printf("Size of a long long int: %lu.\n", (unsigned long)sizeof(c));
+	printf("Size of float is: %ld", sizeof(floatType));
+
+	return (0);
+}
+
