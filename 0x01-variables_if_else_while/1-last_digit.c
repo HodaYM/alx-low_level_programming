f5434c18c415f288b0c54d0cf70f8290c6244c11
“Hoda <hoda27mohamed@gmail.com>
2023-07-15 14:39:55 -0700

    last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
         }
         else if ((n % 10) < 6 && (n % 10) != 0)
         {
-                printf("Last digit of %d is %d and is less than 6 and not 0\n", n % 10, (int)Lat_digit);
+                printf("Last digit of %d is %d and is less than 6 and not 0\n", n % 10, (int)Last_digit);
 	}
         return (0);
 }
