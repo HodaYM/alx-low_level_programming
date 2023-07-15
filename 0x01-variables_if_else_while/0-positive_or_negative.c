4b7c1a3450e4d25805d5067f1bc623bbed722213
“Hoda <hoda27mohamed@gmail.com>
2023-07-14 18:05:11 -0700

    positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,17 +6,17 @@
  */
 int main(void)
 {
-	int n:
+	int n;
 	       if (n > 0)
 	       {
 	printf(" %d is positive\n", n);
 	       }
 	       else if (n == 0)
 	       {
-	printf(" %d  is zero\n", n);
+	printf("%d is zero\n", n);
 	       }
 	       else
-	       {printf(" %d is negative\n", n);
+	       {printf("%d is negative\n", n);
 	       }
 	return (0);
 }
