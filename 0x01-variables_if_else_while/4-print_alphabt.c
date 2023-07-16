eb5c443420bd267e3c3c6d9b53aacbbb0f65bdce
“Hoda <hoda27mohamed@gmail.com>
2023-07-16 00:45:35 -0700

    print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * * main - Entry point
+ *
+ * prints the alphabet in lowercase except q and e in lowercase
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++) 
+{
+	if (c != 'q' && c != 'e') 
+{
+putchar(c);
+}
+putchar('\n');
+}
+return (0);
+}
