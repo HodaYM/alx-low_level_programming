cc88d64a7cb8cf918a14588e1e3029f78c1c9ce7
“Hoda <hoda27mohamed@gmail.com>
2023-07-15 20:58:27 -0700

    print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * * main - Entry point
+ *
+ * It prints the alphabet in lowercase then in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+{
+	       putchar(c);	
+{
+	if (c <= 'z')
+{
+		putchar('n');
+}
+	for (c = 'A'; c <= 'Z'; c++);
+{
+		putchar(c);
+	return (0);
+}
