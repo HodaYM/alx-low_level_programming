4cab0454dbfa7851e4fd57dd26f2b36093b606ae
“Hoda <hoda27mohamed@gmail.com>
2023-07-16 18:41:25 -0700

    print_base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * * main - Entry point
+ *
+ * Print all the numbers in lowercase
+ *
+ * Return:Always = 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char c = i + 0;
+
+	for (i = 0; i <= 15; i++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
