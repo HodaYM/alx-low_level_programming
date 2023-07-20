1a80ed7cf38358c922c9638b5f7f204984db74ce
“Hoda <hoda27mohamed@gmail.com>
2023-07-19 18:05:34 -0700

    Natural_numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,16 +11,37 @@
 void print_to_98(int n)
 
 {
-int i = 0;
 
-for (i = n; i <= 98; i++)
+if (n <= 98)
 {
-putchar(i + '0');
-if (i != 98)
+for (; n <= 98; n++)
 {
-putchar(',');
-putchar(' ');
+if (n == 98)
+{
+printf("%d", n);
+printf("\n");
+break;
 }
-putchar('\n');
+else
+{
+printf("%d, ", n);
+}
+}
+}
+else
+{
+for (; n >= 98; n--)
+{
+if (n == 98)
+{
+printf("%d", n);
+printf("\n");
+break;
+}
+else
+{
+printf("%d, ", n);
+}
+}
 }
 }
