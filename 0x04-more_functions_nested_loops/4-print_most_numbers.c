92fd70ef483a01c99b6c744fab6fa2f98489a8be
“Hoda <hoda27mohamed@gmail.com>
2023-07-24 14:44:03 -0700

    prints_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,7 +8,7 @@
 void print_most_numbers(void)
 {
 int x = 0;
-for (; x = 0; x <= 9; x++)
+for (; x <= 9; x++)
 {
 if (x == 2 || x == 4)
 {
@@ -16,7 +16,7 @@ continue;
 }
 else
 {
-_Putchar(x + '0');
+_putchar(x + '0');
 }
 }
 _putchar('\n');
