6cb33b56a01ff5ffdffafd9affee3364a29d5b63
“Hoda <hoda27mohamed@gmail.com>
2023-07-23 20:06:38 -0700

    Upper_letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - Function that checks for upper character
+ *  @c: The number to be checked
+ *  Return: 1 for upper letters or 0 if any else
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
