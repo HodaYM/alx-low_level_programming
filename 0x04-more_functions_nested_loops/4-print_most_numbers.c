da9999ec9cd9c565d2d2fc6f86d2f28d7aa5e30f
“Hoda <hoda27mohamed@gmail.com>
2023-07-24 14:02:08 -0700

    print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_most_numbers - Function that prints the numbers from 0 to 9 and exclude 2 and 4
+* Return: The printed numbers from 0 to 9
+* */
+void print_most_numbers(void)
+{
+int x = 0;
+for (;x = 0; x <= 9; x++)
+{
+if (x == 2 || x == 4)
+{
+continue;
+}
+else
+{
+_Putchar(x + '0');
+}
+}
+_putchar('\n');
+}
