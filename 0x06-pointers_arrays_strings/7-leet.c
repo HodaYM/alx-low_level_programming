4ba29b732c3f79cd4750ad79e5f5380c6d8957f2
Hoda Mohamed <hoda27mohamed@gmail.com>
2023-07-27 19:28:52 -0700

    leet encodes a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - A function encodes a string into 1337
+ * @s: The string to encode
+ * Return: The encode string
+ */
+char *leet(char *s)
+{
+int a = 0, b = 0, l = 5;
+char x[5] = {'A', 'E', 'O', 'T', 'L'};
+char y[5] = {'4', '3', '0', '7', '1'};
+while (s[a])
+{
+b = 0;
+while (b < l)
+{
+if (s[a] == x[b] || s[a] - 32 == x[b])
+{
+s[a] = y[b];
+}
+b++;
+}
+a++;
+}
+return (s);
+}
