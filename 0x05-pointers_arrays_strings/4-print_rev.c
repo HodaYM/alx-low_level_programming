011bb865dcfa43f7ca8c20b11444edd35f2b589b
Hoda Mohamed <hoda27mohamed@gmail.com>
2023-07-30 17:19:13 -0700

    reverse a string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,13 @@
 void print_rev(char *str)
 {
 int c = 0;
-while (s[c] != ('\0'))
+while (str[c] != ('\0'))
 {
 c++;
 }
 for (c -= 1; c >= 0; c--)
 {
-_putchar(s[c]);
+_putchar(str[c]);
 }
-_putchar('\n')
+_putchar('\n');
 }
