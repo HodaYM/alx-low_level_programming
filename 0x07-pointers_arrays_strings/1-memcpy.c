a6aafecb2e06add86e913e0c6074244952d0bea8
Hoda Mohamed <hoda27mohamed@gmail.com>
2023-07-31 17:08:53 -0700

    _memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,8 +4,8 @@
  *  _memcpy - Entry point
  *  @src: The source that will be copied from
  *  @n: The bytes
- * @dest: the pointed destnation
- * Return: 0 Always (Success)
+ * @dest: destenation
+ * Return: Always 0 (Success)
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
