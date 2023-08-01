20ae6ec6f88b4e0a8e262e37cf6b1eee03f56d66
Hoda Mohamed <hoda27mohamed@gmail.com>
2023-07-31 17:18:55 -0700

    _memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,9 +2,9 @@
 #include <string.h>
 /**
  *  _memcpy - Entry point
+ *  @dest: input
  *  @src: input
- *  @n: input
- * @dest: input
+ * @n: input
  * Return: Always 0 (Success)
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
