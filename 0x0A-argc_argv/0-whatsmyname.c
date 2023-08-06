81d265fb8f052e277c525e6bcf48b43516ee8fe2
Hoda Mohamed <hoda27mohamed@gmail.com>
2023-08-06 00:34:09 -0700

    print the name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - A program that prints its program name
+ * @argc: Arugments counts
+ * @argv: Arugments vectors
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
