f04f29c13ff363205fff4a59849c567e2f7d14f0
Hoda Mohamed <hoda27mohamed@gmail.com>
2023-08-06 16:33:53 -0700

    add positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,8 +14,7 @@ while (--argc)
 {
 for (c = argv[argc]; *c; c++)
 if (*c < 48 || *c > 57)
-printf("Error\n");
-return (1);
+return (printf("Error\n"), 1);
 sum += atoi(argv[argc]);
 }
 printf("%d\n", sum);
