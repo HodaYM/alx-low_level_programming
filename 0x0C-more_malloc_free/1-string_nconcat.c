7a9b5eeeef33dc8da882735d3a90163ad33d7e69
HodaYM <hoda27mohamed@gmail.com>
2023-08-23 17:07:40 -0700

    concatenates two strings2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,21 +12,26 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *str;
 unsigned int l, k, s1_len, s2_len;
+/* checking if the strings passing null */
 if (s1 == NULL)
 s2 = "";
 if (s2 == NULL)
 s2 = "";
+/* computing the length of the strings  */
 for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
 ;
 for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
 ;
+/* reserve memory for cases 1 & 2 */
 str = malloc(s1_len + n + 1);
 if (str == NULL)
 {
 return (NULL);
 }
+/* copy the first string into str */
 for (l = 0; s1[l] != '\0'; l++)
 str[l] = s1[l];
+/*copy the second string into str */
 for (k = 0; k < n; k++)
 {
 str[l] = s2[k];
